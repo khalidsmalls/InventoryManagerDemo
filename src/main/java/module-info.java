195d047952837e91257e381ccdc1d836702c8a90
khalid <khalidsmalls@protonmail.com>
2024-02-28 00:40:50 -0500

    export model package

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,9 +2,10 @@ module com.smalls.inventorymanagerdemo {
     requires javafx.controls;
     requires javafx.fxml;
 
-
+    opens com.smalls.inventorymanagerdemo.model to javafx.base;
     opens com.smalls.inventorymanagerdemo to javafx.fxml;
     exports com.smalls.inventorymanagerdemo;
     exports com.smalls.inventorymanagerdemo.controller;
+    exports com.smalls.inventorymanagerdemo.model;
     opens com.smalls.inventorymanagerdemo.controller to javafx.fxml;
 }
